6bd314e84603f49c6f868b0d343b59bfcd5e1a3c
vanderleysviana <vanderleysviana@gmail.com>
2025-11-23 23:42:07 -0300

    Update patients.js

--- src/patients.js
+++ src/patients.js
@@ -2,5 +2,5 @@
 // facade that re-exports patient-related API
 
 export { initPatientUI, openPatientManager, selectPatient, renderPatientList, openEditPatientForm } from './patientManager.js';
-export { openPatientDiets, createNewDietForPatient, saveCurrentSessionAsDiet, loadDietToSession, saveEditingDiet } from './patientDiets.js';
+export { openPatientDiets, createNewDietForPatient, saveCurrentDiet, loadDietToSession, saveCurrentSessionAsDiet } from './patientDiets.js';
 export { openPatientConsultations } from './consultations.js';
