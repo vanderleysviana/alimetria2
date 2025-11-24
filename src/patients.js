79bb87bb3b0e9741978e8473ec84b356349bcbb6
vanderleysviana <vanderleysviana@gmail.com>
2025-11-24 11:24:02 -0300

    Update patients.js

--- src/patients.js
+++ src/patients.js
@@ -1,6 +1,22 @@
-// src/patients.js - FACADE VERIFICADO
-// facade that re-exports patient-related API
+// src/patients.js - FACADE SIMPLIFICADO
+// Este arquivo agora serve apenas como facade para evitar importações circulares
 
-export { initPatientUI, openPatientManager, selectPatient, renderPatientList, openEditPatientForm } from './patientManager.js';
-export { openPatientDiets, createNewDietForPatient, saveCurrentSessionAsDiet, loadDietToSession, saveEditingDiet, saveCurrentDiet } from './patientDiets.js';
+export { 
+  initPatientUI, 
+  openPatientManager, 
+  selectPatient, 
+  renderPatientList, 
+  openEditPatientForm,
+  deletePatient 
+} from './patientManager.js';
+
+export { 
+  openPatientDiets, 
+  createNewDietForPatient, 
+  saveCurrentSessionAsDiet, 
+  loadDietToSession, 
+  saveEditingDiet, 
+  saveCurrentDiet 
+} from './patientDiets.js';
+
 export { openPatientConsultations } from './consultations.js';
